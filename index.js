64fb746d96bc39953eb8a445b6231cdd86b0b198
ajlic24@gmail.com <ajlic24@gmail.com>
2021-11-23 11:17:44 -0500

    commit tree 7f846993796b04bff9468d8561b2b4fcb4f3c555

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.round(Math.random()*100);
+
+let num3 = 20;
+let num4 = 8;
+
+const mod = num3 % num4;
+
+const max = Math.max(1, 3, 14, 20);
